2a73b3edb22edb7f6e798a501c82de297bca5c74
web-ghost-thiru <thirumurugankutty@gmail.com>
2024-12-02 13:06:05 +0530

    Layout change

---
+++ src/components/AddDoctor.tsx
@@ -0,0 +1,9 @@
+const AddDoctor = () => {
+    console.log("ADD doctor");
+    
+  return (
+    <div>AddDoctor</div>
+  )
+}
+
+export default AddDoctor
---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import Home from './Home'
+import Sidebar from './Sidebar'
+
+const Layout = () => {
+  return (
+    <>
+        <Sidebar />
+        <Home />
+    </>
+  )
+}
+
+export default Layout
