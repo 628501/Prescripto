c09cb0f83bb33200d515ecdd195bd118cde55cd1
Kartheeswaran_16 <karthis15cse@gmail.com>
2024-12-03 18:54:17 +0530

    session updated

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -12,17 +12,28 @@ import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined
 import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 const drawerWidth = 280;
+
 const Sidebar = () => {
+  const [selected, setSelected] = useState<string>("");
+
+  useEffect(() => {
+    const savedState = sessionStorage.getItem("state:");
+    if (savedState) {
+      setSelected(savedState);
+    }
+  }, []);
   
-  const [selected, setSelected] = useState<string | undefined>("");
+  useEffect(() => {
+    if (selected) {
+      sessionStorage.setItem("state:", selected);
+    }
+  }, [selected]);
 
   return (
-    <Box
-      component="nav"
-      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-    >
+    <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}>
       <Drawer
         variant="permanent"
         sx={{
@@ -37,16 +48,12 @@ const Sidebar = () => {
         <Toolbar />
         <Divider />
         <List>
-          <Link
-            to="dashboard"
-            style={{ textDecoration: "none", color: "black" }}
-          >
+          <Link to="dashboard" style={{ textDecoration: "none", color: "black" }}>
             <ListItem
               disablePadding
               sx={{
                 color: "gray",
-                borderRight:
-                  selected === "dashboard" ? "4px solid #5F6FFF" : "",
+                borderRight: selected === "dashboard" ? "4px solid #5F6FFF" : "",
                 background: selected === "dashboard" ? "#F2F3FF" : "",
               }}
             >
@@ -59,16 +66,12 @@ const Sidebar = () => {
             </ListItem>
           </Link>
 
-          <Link
-            to="appointments"
-            style={{ textDecoration: "none", color: "black" }}
-          >
+          <Link to="appointments" style={{ textDecoration: "none", color: "black" }}>
             <ListItem
               disablePadding
               sx={{
                 color: "gray",
-                borderRight:
-                  selected === "appointments" ? "4px solid #5F6FFF" : "",
+                borderRight: selected === "appointments" ? "4px solid #5F6FFF" : "",
                 background: selected === "appointments" ? "#F2F3FF" : "",
               }}
             >
@@ -81,16 +84,12 @@ const Sidebar = () => {
             </ListItem>
           </Link>
 
-          <Link
-            to="add-doctor"
-            style={{ textDecoration: "none", color: "black" }}
-          >
+          <Link to="add-doctor" style={{ textDecoration: "none", color: "black" }}>
             <ListItem
               disablePadding
               sx={{
                 color: "gray",
-                borderRight:
-                  selected === "add-doctor" ? "4px solid #5F6FFF" : "",
+                borderRight: selected === "add-doctor" ? "4px solid #5F6FFF" : "",
                 background: selected === "add-doctor" ? "#F2F3FF" : "",
               }}
             >
@@ -103,16 +102,12 @@ const Sidebar = () => {
             </ListItem>
           </Link>
 
-          <Link
-            to="doctors-list"
-            style={{ textDecoration: "none", color: "black" }}
-          >
+          <Link to="doctors-list" style={{ textDecoration: "none", color: "black" }}>
             <ListItem
               disablePadding
               sx={{
                 color: "gray",
-                borderRight:
-                  selected === "doctors-list" ? "4px solid #5F6FFF" : "",
+                borderRight: selected === "doctors-list" ? "4px solid #5F6FFF" : "",
                 background: selected === "doctors-list" ? "#F2F3FF" : "",
               }}
             >
